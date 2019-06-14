aadcc98bebc90cad6043227258cc913d660a6814
Aron Polner <aron.polner@gmail.com>
2019-06-14 16:48:32 +0200

    Working AR project with physics and occlusion detection implemented.

---
+++ arcourse/Assets/MatScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MatScript : MonoBehaviour
+{
+    Color[] colors;
+    Renderer thisRend;
+    float transitionTime = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRend = GetComponent<Renderer>();
+        colors = new Color[6];
+        //colors[0] = Color.white;
+        colors[0] = new Color(0.3f, 0.4f, 0.6f, 0.3f);
+        colors[1] = Color.white;
+        colors[2] = Color.white;
+        colors[3] = Color.white;
+        colors[4] = new Color(0.3f, 0.4f, 0.6f, 0.3f);
+        colors[5] = new Color(0.3f, 0.4f, 0.6f, 0.3f);
+        StartCoroutine(ColorChange());
+    }
+
+    IEnumerator ColorChange()
+    {
+        while(true)
+        {
+            Color newColor = colors[(UnityEngine.Random.Range(0, 5))];
+            float transitionRate = 0;
+
+            while (transitionRate < 1)
+            {
+                thisRend.material.SetColor("_Color", Color.Lerp(thisRend.material.color, 
+                    newColor, Time.deltaTime * transitionRate));
+                transitionRate += Time.deltaTime / transitionTime;
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ arcourse/Assets/MobileARCourse/Scripts/ARLessons.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class ARLessons : MonoBehaviour
+{
+    [SerializeField] TMPro.TMP_Text stateText;
+    [SerializeField] TMPro.TMP_Text numberOfPlanesText;
+    [SerializeField] ARPlaneManager planeManager;
+    [SerializeField] ARPointCloudManager pointCloudManager;
+    //[SerializeField] Transform transformVariable;
+    [SerializeField] ARRaycastManager raycastManager;
+    [SerializeField] GameObject prefabRobot;
+    [SerializeField] ARCameraManager cameraManager;
+    [SerializeField] GameObject ballPrefab;
+
+    private GameObject robot;
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    private Vector3 fingerPostion;
+    private int planeAdded = 0;
+    private int planeRemoved = 0;
+    private bool placementPoseIsValid = false;
+
+    // GPS variables
+    private float Latitude;
+    private float Longitude;
+
+    void Start()
+    {
+        ARSession.stateChanged += ARSession_StateChanged;
+        planeManager.planesChanged += PlaneManager_PlanesChanged;
+        pointCloudManager.pointCloudsChanged += PointCloudManager_PointCloudsChanged;
+        
+        // GPS
+        //if (Input.location.isEnabledByUser)
+        StartCoroutine(GetLocation());
+
+    }
+
+    private IEnumerator GetLocation()
+    {
+        Input.location.Start();
+        while (Input.location.status == LocationServiceStatus.Initializing)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Latitude = Input.location.lastData.latitude;
+        Longitude = Input.location.lastData.longitude;
+        yield break;
+
+    }
+    public void ShootBall()
+    {
+        GameObject theball = Instantiate(ballPrefab);
+        theball.transform.position = Camera.main.transform.position;
+        var rb = theball.GetComponent<Rigidbody>();
+        rb.AddForce(5000 * Camera.main.transform.forward);
+    }
+
+    void PointCloudManager_PointCloudsChanged(ARPointCloudChangedEventArgs obj)
+    {
+        //Debug.Log("point cloud");
+    }
+
+    // Added planes accumulate as the app runs
+    // https://docs.unity3d.com/Packages/com.unity.xr.arfoundation@1.0/api/UnityEngine.XR.ARFoundation.ARPlaneManager.html?q=planes
+
+    void PlaneManager_PlanesChanged(ARPlanesChangedEventArgs obj)
+    {
+        //Debug.Log("planes changed");
+        planeAdded += obj.added.Count;
+        planeRemoved += obj.removed.Count;
+        //numberOfPlanesText.text = "planes added: " + planeAdded + "\nplanes removed: " + planeRemoved;
+    }
+
+
+    void ARSession_StateChanged(ARSessionStateChangedEventArgs obj)
+    {
+        //Debug.Log("state");
+        stateText.text = obj.state.ToString();  
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+
+            Touch touch = Input.GetTouch(0);
+            bool hitsFound = raycastManager.Raycast(touch.position, s_Hits, TrackableType.Planes);
+
+            // https://github.com/Unity-Technologies/arfoundation-samples/blob/master/Assets/Scripts/PlaceOnPlane.cs
+            // line 46-61
+            // currently at: Step 2 -> 3.
+            
+            // light estimation callbacks!
+
+            if (hitsFound)
+            {
+                var hit = s_Hits[0];
+                Vector3 newRobotPosition = hit.pose.position;
+                Quaternion newRobotRotation = hit.pose.rotation;
+
+                //robot = Instantiate(prefabRobot, newRobotPosition, newRobotRotation);
+                if (robot == null)
+                {
+                    robot = Instantiate(prefabRobot, newRobotPosition, newRobotRotation);
+                    var rb = robot.GetComponent<Rigidbody>();
+                    rb.isKinematic = false;
+                    // rb.detectCollisions = true;
+                    rb.velocity = Vector3.zero;
+                    rb.angularVelocity = Vector3.zero;
+                }
+                else
+                {
+                    robot.transform.position = newRobotPosition;
+                    robot.transform.rotation = newRobotRotation;
+                }
+
+            }
+            // test som skriver ut x, y av finger på skärm
+            //Vector2 touchPosition = touch.position;
+            //numberOfPlanesText.text = $"x: {touchPosition.x} \ny: {touchPosition.y}";
+            //numberOfPlanesText.text = $"hits baby: {s_Hits.Count}";
+        }
+        Latitude = Input.location.lastData.latitude;
+        Longitude = Input.location.lastData.longitude;
+        numberOfPlanesText.text = $"Latitude: {Latitude}\nLongitude: {Longitude}";
+
+    }
+}
